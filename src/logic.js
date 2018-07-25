c6d8276052d512be01f9100bd4672be10677db61
Afal <afal@live.com>
2018-07-25 14:55:53 +0300

    adjusted logic function

--- src/logic.js
+++ src/logic.js
@@ -1,15 +1,8 @@
-// var mainJson = {ahmad:'ahmad',marwan:'marwan',asma:'asma',ali:'ali',lubna:'lubna'}
-// var mainJson = require("./html.json");
-
 
 function filterJson(arr, like) {
-  var newarr = [];
-  arr.forEach(function(elem,index) {
-    if (elem.indexOf(like) !== -1) 
-    newarr.push(elem);
+return arr.filter(function(elem){
+    return elem.indexOf(like) !== -1
   });
-  return newarr;
 }
 
-// filterJson(mainJson,'h');
 module.exports = filterJson;
---
+++ src/test.js
@@ -0,0 +1,9 @@
+var test = require('tape');
+var filterarr = require('./logic');
+
+
+test('test no 1', function(t){
+var expected = filterJson() ;
+var actual = ;
+t.deepEqual()
+});
